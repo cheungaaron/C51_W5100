5488600fd25e3524199dbf8a101e80aba8dae17e
aaron <Zhian.Zhang@outlook.com>
2020-05-01 13:15:58 +0800

    test

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include<stdio.h>
 #include"test.h"
-extern void swaptest(int a,int b);
+void swaptest(int a,int b);
 int main(void)
 {
     int a = 2;
     int b = 3;
     swaptest(a,b);
+    system("pause");
+    return 0;
+}
+void swaptest(int a, int b)
+{
+    int i;
+    i = a;
+    a = b;
+    b = i;
+    printf("%d\n%d\n",a,b);
 }
--- test.h
+++ test.h
@@ -1,4 +1,1 @@
-#ifndef _TEST_H_
-#define _TEST_H_
 void swaptest(int a,int b);
-#endif
